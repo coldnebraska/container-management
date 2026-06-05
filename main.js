994990c975c93365417c6ab8da2dbf85883046f5
David Brush <brush.david99@gmail.com>
2026-06-05 09:33:37 -0500

    added basic application window code

---
+++ main.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const { app, BrowserWindow, Menu } = require('electron')
+
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Container Manager',
+        width: isDev ? 1000 : 500,
+        height: 600
+    })
+
+    // Open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Container Manager',
+        width: isDev ? 1000 : 500,
+        height: 600
+    })
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'))
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+
+    // implement menu
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows.length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+// Menu template ---- blank
+const menu = [
+    // ...(isMac ? [{
+    //     label: app.name,
+    //     submenu: [
+    //         {
+    //             label: 'About',
+    //             click: createAboutWindow
+    //         }
+    //     ]
+    // }] : []),
+    // {
+    //     role: 'fileMenu'
+    // },
+    // ...(!isMac ? [
+    //     {
+    //         label: 'Help',
+    //         submenu: [
+    //             {
+    //                 label: 'About',
+    //                 click: createAboutWindow
+    //             }
+    //         ]
+    //     }
+    // ] : [])
+]
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
