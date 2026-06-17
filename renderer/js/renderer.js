8f16df901309a9f98d46d9e1d9e101664e67d412
David Brush <brush.david99@gmail.com>
2026-06-17 08:30:00 -0500

    added item selection with highlighting and count

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,6 +1,34 @@
-const button = document.getElementById('override-dimensions')
-    const fieldset = document.getElementById('dimension-inputs')
+let items = []
+let selectedItems = []
 
-    button.addEventListener('click', () => {
-        fieldset.disabled = false
+// override dimensions
+const button = document.getElementById('override-dimensions')
+const fieldset = document.getElementById('dimension-inputs')
+
+button.addEventListener('click', () => {
+    fieldset.disabled = false
+    button.disabled = true
 })
+
+// item selection
+const itemList = document.getElementsByClassName('item')
+const count = document.getElementById('item-count')
+
+for (i = 0; i < itemList.length; i++) {
+    items.push(itemList[i])
+}
+
+items.forEach(item => {
+    item.addEventListener('change', () => {
+        if (!selectedItems.includes(item)) {
+            selectedItems.push(item)
+            item.id = "selected"
+        } else {
+            selectedItems = selectedItems.filter(filterItem => filterItem !== item)
+            item.id = ''
+        }
+
+        count.innerHTML = selectedItems.length
+    })
+})
+
