f1ecaff537c5567413c51a011380f267a99898e9
David Brush <brush.david99@gmail.com>
2026-09-01 22:55:30 -0500

    added dynamic rrma item population based on selected container

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,5 +1,6 @@
 let items = []
 let selectedItems = []
+let selectedContainer = document.getElementById('selected-container')
 
 // override dimensions
 const button = document.getElementById('override-dimensions')
@@ -9,6 +10,89 @@ button.addEventListener('click', () => {
     fieldset.disabled = false
     button.disabled = true
 })
+
+// dummy data for testing
+const dummyItems = [
+    {
+        container: 'blue',
+        rrma: 'D3xhH49RRMA',
+        date: 'Thurs 6/4'
+    },
+    {
+        container: 'green',
+        rrma: 'DyF7lBwRRMA',
+        date: 'Fri 6/5'
+    },
+    {
+        container: 'red',
+        rrma: 'D9WlB4wRRMA',
+        date: 'Fri 6/12'
+    }
+]
+
+// container selection
+function selectContainer() {
+    const containerList = document.getElementsByClassName('destination-container')
+    const containers = []
+
+    for (i = 0; i < containerList.length; i++) {
+        containers.push(containerList[i])
+    }
+
+    containers.forEach(container => {
+        container.addEventListener('click', () => {
+            if (selectedContainer.id === 'selected-container') {
+                selectedContainer.id = ''
+            }
+            
+            if (selectedContainer != container) {
+                for (i = 0; i < itemList.length; i++) {
+                    itemList[i].firstElementChild.checked = false
+                    itemList[i].id = ''
+                }
+                
+                selectedItems = []
+                count.innerHTML = selectedItems.length
+            }
+
+            selectedContainer = container.parentElement
+            container.parentElement.id = 'selected-container'
+
+            clearItems()
+            populateItems(dummyItems)
+        })
+    })
+
+    populateItems(dummyItems)
+}
+
+// dynamic item population
+function populateItems(items) {
+    const itemList = document.getElementById('item-list')
+
+    items.forEach(item => {
+        if (item.container == selectedContainer.className) {
+            const itemContainer = document.createElement('div')
+            itemContainer.className = 'item'
+            itemContainer.innerHTML = `
+                <input type="checkbox">
+                <p id="rrma">${item.rrma}</p>
+                <p>${item.date}</p>
+            `
+            itemList.appendChild(itemContainer)
+        }
+    })
+}
+
+// clear items
+function clearItems() {
+    const itemList = document.getElementById('item-list')
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
+selectContainer()
 
 // item selection
 const itemList = document.getElementsByClassName('item')
@@ -32,32 +116,3 @@ items.forEach(item => {
     })
 })
 
-// container selection
-const containerList = document.getElementsByClassName('destination-container')
-const containers = []
-let selectedContainer = document.getElementById('selected-container')
-
-for (i = 0; i < containerList.length; i++) {
-    containers.push(containerList[i])
-}
-
-containers.forEach(container => {
-    container.addEventListener('click', () => {
-        if (selectedContainer.id === 'selected-container') {
-            selectedContainer.id = ''
-        }
-        
-        if (selectedContainer != container) {
-            for (i = 0; i < itemList.length; i++) {
-                itemList[i].firstElementChild.checked = false
-                itemList[i].id = ''
-            }
-            
-            selectedItems = []
-            count.innerHTML = selectedItems.length
-        }
-
-        selectedContainer = container
-        container.id = 'selected-container'
-    })
-})
