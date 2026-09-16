b180876f4e12cda00cead93bf27f8d7240a7d555
David Brush <brush.david99@gmail.com>
2026-09-15 21:15:33 -0500

    updated styling and added form submission to get rrma values from user input

---
+++ renderer/js/dropoff.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('dropoff-form')
+const input = document.getElementById('dropoff-input')
+let rrmaInputData = {
+    rrma: '',
+    date_created: ''
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    rrmaInputData = {
+        rrma: input.value,
+        date_created: Date.now()
+    }
+
+    input.value = ''
+})
